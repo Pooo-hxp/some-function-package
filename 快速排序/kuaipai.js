2fb54dce1c8b1933476e5c225249c53804336615
GitHpotato <hengxipeng@qq.com>
2019-09-07 23:52:32 +0800

    优化代码，更改为面向对象编程

--- 快速排序/kuaipai.js
+++ 快速排序/kuaipai.js
@@ -5,24 +5,45 @@
         show.innerHTML=quickSort(arr);
         console.log(quickSort(arr));
     }
-    
-    var arr=[1,7,3,4,8,9,10];
+    /**JS利用面向对象编程 */
+(function(window){
     function quickSort(arr){
-        return arr.length<=1?arr:hxp(arr);
+        return arr.length<=1?arr:quickSort.prototype.init(arr);
+        /* 更改返回值*/
     }
+    quickSort.prototype={
+        constructor:quickSort,
+        init:function(arr){
+            this.arr=arr;
+           /**找到需快排数组中间的数字的下标*/
+            let flag=Math.floor(arr.length/2);
+            let key=arr.splice(flag,1);
+            /**取出数组项为基准,使用arr[num]方式也能获取*/
+            let left=[];
+            let right=[];
+            for(let i=0;i<arr.length;i++){
+                /*数组与基准比较，大在右小在左*/
+                arr[i]>key?right.push(arr[i])
+                :left.push(arr[i]);
+            }
+              /* 迭代重复排序并利用数组concat方法进行数据拼接 **/
+            return  quickSort(left).concat(key, quickSort(right));
+        }
+    }
+    window.quickSort=quickSort;
+})(window) 
+/**JS常用写法 */
+/**
     function hxp(arr){
-        /**找到需快排数组中间的数字的下标*/
         var flag=Math.ceil(arr.length/2);
-        /**取出数组项为基准*/
         var key=arr.splice(flag,1);
         var left=[];
         var right=[];
-        /**比这个基准小的放左数组，大的放右数组*/
         for(let i=0;i<arr.length;i++){
             arr[i]>key?right.push(arr[i])
             :left.push(arr[i]);
-        }
-        /*迭代重复排序并拼接**/
+        }     
         return quickSort(left).concat(key,quickSort(right));
     }
+ **/
    
