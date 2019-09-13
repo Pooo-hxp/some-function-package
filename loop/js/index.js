33ee8112b82e6106c9aadaa219e7ca190d914feb
GitHpotato <hengxipeng@qq.com>
2019-09-13 23:34:56 +0800

    引入jquery

---
+++ loop/js/index.js
@@ -0,0 +1,8 @@
+(function(window){
+    const lf=$("#prev");
+    const rg=$("#next");
+    let $flag=$("#list>li");
+    lf.click(function(){
+        // console.log($flag[4].fadeIn());
+    })
+})(window);
