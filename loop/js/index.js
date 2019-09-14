a3fe923141b67ceef2adc17ab45163deb6d33ebf
GitHpotato <hengxipeng@qq.com>
2019-09-14 15:42:56 +0800

    添加左右控件及下部标识功能

--- loop/js/index.js
+++ loop/js/index.js
@@ -1,8 +1,31 @@
 (function(window){
     const lf=$("#prev");
     const rg=$("#next");
-    let $flag=$("#list>li");
+    let $li=$("#list>li");
+    let $dot=$("#dot>li");
+    var flag=0;
+    $li.eq(flag).fadeIn().siblings().fadeOut();
     lf.click(function(){
-        // console.log($flag[4].fadeIn());
+        flag==0?flag=4:flag--;
+        $li.eq(flag).fadeIn().siblings().fadeOut();
+        $dot.eq(flag).addClass("active").siblings().removeClass("active");
     })
+    rg.click(function(){
+        flag==4?flag=0:flag++;
+        $li.eq(flag).fadeIn().siblings().fadeOut();
+        $dot.eq(flag).addClass("active").siblings().removeClass("active");
+    }) 
+    var bystop=window.setInterval(settime,2000);
+    function settime(){
+        flag==4?flag=0:flag++;
+    $dot.eq(flag).addClass("active").siblings().removeClass("active");
+    $li.eq(flag).fadeIn().siblings().fadeOut();
+    }
+    $("#box").mouseover(function(){
+       clearTimeout(bystop);
+    })
+    $("#box").mouseleave(function(){
+        /**不使用var声明，暴露给外界，覆盖之前定时器，不然会叠加 */
+      bystop=window.setInterval(settime, 2000)
+     })
 })(window);
