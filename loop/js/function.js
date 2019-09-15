5d4f05802dbea3ac5eda81c90a95a7cb41cb9fc0
GitHpotato <hengxipeng@qq.com>
2019-09-15 14:37:49 +0800

    分离封装的轮播图方法，并在子文件index中引用此方法

---
+++ loop/js/function.js
@@ -0,0 +1,55 @@
+(function(window){
+    let $li=$("#list>li");
+    let $dot=$("#dot>li");
+    function Loop(){};
+    var flag=0;
+/**面向对象写法 */
+    Loop.prototype={
+        constructor:Loop,
+        lfun:function(){
+            flag==0?flag=4:flag--;
+            $li.eq(flag).fadeIn().siblings().fadeOut();
+            $dot.eq(flag).addClass("active").siblings().removeClass("active");
+        },
+        rfun:function(){
+            flag==4?flag=0:flag++;
+            $li.eq(flag).fadeIn().siblings().fadeOut();
+            $dot.eq(flag).addClass("active").siblings().removeClass("active");
+            console.log(flag);
+        },
+        timer:function(){
+            console.log("开启轮播");
+         settime= setInterval(Loop.prototype.rfun,2000)
+        },
+        removetimer:function(){
+            console.log("移除定时器"); 
+            clearInterval(settime);
+        }
+    };
+    window.Loop=Loop;
+})(window);
+/**下方为面向过程写法 */
+    /*lf.click(function(){
+        flag==0?flag=4:flag--;
+        $li.eq(flag).fadeIn().siblings().fadeOut();
+        $dot.eq(flag).addClass("active").siblings().removeClass("active");
+    })
+    rg.click(function(){
+        flag==4?flag=0:flag++;
+        $li.eq(flag).fadeIn().siblings().fadeOut();
+        $dot.eq(flag).addClass("active").siblings().removeClass("active");
+    }) 
+    var bystop=window.setInterval(settime,2000);
+    function settime(){
+        flag==4?flag=0:flag++;
+        $li.eq(flag).fadeIn().siblings().fadeOut();
+        $dot.eq(flag).addClass("active").siblings().removeClass("active");
+    }
+    $("#box").mouseover(function(){
+       clearTimeout(bystop);
+    })
+    $("#box").mouseleave(function(){
+        //不使用var声明，暴露给外界，覆盖之前定时器，不然会叠加 
+      bystop=window.setInterval(settime, 2000)
+     })
+     */
--- loop/js/index.js
+++ loop/js/index.js
@@ -1,31 +1,7 @@
-(function(window){
-    const lf=$("#prev");
-    const rg=$("#next");
-    let $li=$("#list>li");
-    let $dot=$("#dot>li");
-    var flag=0;
-    $li.eq(flag).fadeIn().siblings().fadeOut();
-    lf.click(function(){
-        flag==0?flag=4:flag--;
-        $li.eq(flag).fadeIn().siblings().fadeOut();
-        $dot.eq(flag).addClass("active").siblings().removeClass("active");
-    })
-    rg.click(function(){
-        flag==4?flag=0:flag++;
-        $li.eq(flag).fadeIn().siblings().fadeOut();
-        $dot.eq(flag).addClass("active").siblings().removeClass("active");
-    }) 
-    var bystop=window.setInterval(settime,2000);
-    function settime(){
-        flag==4?flag=0:flag++;
-    $dot.eq(flag).addClass("active").siblings().removeClass("active");
-    $li.eq(flag).fadeIn().siblings().fadeOut();
-    }
-    $("#box").mouseover(function(){
-       clearTimeout(bystop);
-    })
-    $("#box").mouseleave(function(){
-        /**不使用var声明，暴露给外界，覆盖之前定时器，不然会叠加 */
-      bystop=window.setInterval(settime, 2000)
-     })
-})(window);
+    const control=new Loop;
+    control.timer();
+    console.log(Loop.prototype.constructor===Loop);
+    document.getElementById("box").addEventListener("mouseover",control.removetimer);
+    document.getElementById("box").addEventListener("mouseleave",control.timer);
+    document.getElementById("prev").addEventListener("click",control.lfun);
+    document.getElementById("next").addEventListener("click",control.rfun); 
