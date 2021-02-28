322b2e200faa1b936cff6266ecf42046364dad80
poo <hengxipeng@qq.com>
2021-02-28 16:08:50 +0800

    补充命名路由

--- VueRouter/Lessono/demo5_命名路由/index.js
+++ VueRouter/Lessono/demo5_命名路由/index.js
@@ -1,3 +1,40 @@
+const User={
+    template:`
+    <div>
+        User {{$route.params.userId}}
+    </div>
+    `
+}
+const Home={
+    template:`
+    <div>
+       <h3>This is  Home</h3> 
+       <button @click='gotoUser'>User</button>
+       <router-link :to="{name:'user',params:{userId:233}}">User</router-link>
+    </div>
+    `,
+    methods:{
+        gotoUser(){
+            this.$router.push({
+                name:'user',
+                params:{userId:'0609'}
+            })
+        }
+    }
+}
+const router=new VueRouter({
+    routes:[
+        {
+            path:'/',
+            component:Home
+        },
+        {
+            name:'user',
+            path:'/user/:userId',
+            component:User
+        }
+    ]
+})
 new Vue({
     el:'#app',
     router:router,
