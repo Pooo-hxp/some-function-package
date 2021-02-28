f03321349ee850997302fa3bbec4cdb69eb0a446
poo <hengxipeng@qq.com>
2021-02-28 14:00:05 +0800

    补充编程式导航

--- VueRouter/Lessono/demo4_编程式导航/index.js
+++ VueRouter/Lessono/demo4_编程式导航/index.js
@@ -1,9 +1,41 @@
+const Layout = {
+    template: `
+    <div>
+       <h2> Index</h2>
+       <router-link to='/home'>go Home</router-link>
+       <router-link to='/category'>go Category</router-link>
+       <router-link to='/shopcart'>go Shopcart</router-link>
+       <router-link to='/setting/233'>go Setting</router-link>
+       <router-view></router-view>
+    </div>
+    `
+}
 const Home = {
     template: `
     <div>
        <h2> Home</h2>
+       <button @click='goToShopcart'>购物车</button>
+       <button @click='goToSetting'>设置页</button>
+       <button @click='goToCategory'>种类</button>
     </div>
-    `
+    `,
+    methods: {
+        goToShopcart() {
+            this.$router.push("Shopcart");
+        },
+        goToSetting() {
+            const userId = '123'
+            //--使用这种方法必须在路由中定义name，否则不生效
+            this.$router.push({ name: 'setting', params: { id: userId } });
+            //--如果提供了 path，params 会被忽略
+            // this.$router.push( {path:`setting/${userId}`});
+
+        },
+        goToCategory() {
+            this.$router.push({ path: 'Category' });
+
+        }
+    }
 }
 const Category = {
     template: `
@@ -23,16 +55,21 @@ const Setting = {
     template: `
     <div>
          <h4> This is Setting </h4>
+         <h6> The userID is {{$route.params.id}} </h6>
     </div>
     `
 }
 const router = new VueRouter({
     routes: [
-        {path: '/',component: Home},
-        {path: '/category',component: Category},
-        {path: '/shopcart',component: Shopcart},
-        {path: '/setting',component: Setting},
-
+        {
+            path: '/', component: Layout,
+            children: [
+                { path: '/home', component: Home },
+                { path: '/category', component: Category },
+                { path: '/shopcart', component: Shopcart },
+                { name:'setting', path: '/setting/:id', component: Setting },
+            ]
+        },
     ]
 })
 new Vue({
