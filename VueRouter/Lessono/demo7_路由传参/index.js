11128d0affb910152a1e9ac9e4eb740e1888592d
poo <hengxipeng@qq.com>
2021-03-01 21:45:22 +0800

    补充命名视图配置

--- VueRouter/Lessono/demo7_路由传参/index.js
+++ VueRouter/Lessono/demo7_路由传参/index.js
@@ -4,17 +4,23 @@ const User = {
     // template: '<div>User {{ $route.params.id }}</div>'
     template: '<div>User {{ id }}</div>'
 };
+const SideBar = {
+    props: ['id'],
+    template: '<div>sidebar {{ id }}</div>'
+};
 const router = new VueRouter({
     routes: [
-        // 对于包含命名视图的路由，你必须分别为每个命名视图添加 `props` 选项：
         // props 被设置为 true，route.params 将会被设置为组件属性
         { path: '/user/:id', component: User, props: true },//---布尔模式
         { path: '/user2/:id', component: User,  props: { id: 222 } },//--对象模式
         { path: '/user3/:id', component: User, props: (route) => ({ id: '333' }) },//--函数模式
         {
-            path: '/user/:id',
-            components: { default: User, sidebar: Sidebar },
-            props: { default: true, sidebar: false }
+        // 对于包含命名视图的路由，必须分别为每个命名视图添加 `props` 选项：
+            path: '/SideBar/:id',
+            components: { default: User, sidebar: SideBar },
+            props: { default: true, sidebar: {
+                title:'this  is sidebar'
+            } }
         }
     ]
 })
