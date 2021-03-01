5e49703165dcbdc541f4e69ac01de70c317339b8
poo <hengxipeng@qq.com>
2021-03-01 21:21:52 +0800

    补充传参内容

--- VueRouter/Lessono/demo7_路由传参/index.js
+++ VueRouter/Lessono/demo7_路由传参/index.js
@@ -1,32 +1,16 @@
-const Home = {
-    template: `
-    <div>
-        User {{$route.params.userId}}
-       <router-link to='a'>jump A</router-link>
-       <router-link to='b'>jump B</router-link>
-       <router-link to='c'>jump C</router-link>
-       <router-link to='d'>jump D</router-link>
-       <router-link to='alias'>jump alias</router-link>
-    </div>
-    `
-}
-const CompB = {
-    template: `
-    <div>
-        This is CompB 
-    </div>
-    `
+const User = {
+    props: ['id'],
+    template: '<div>User {{ id }}</div>'
 }
 const router = new VueRouter({
     routes: [
-        { path: '/', component: Home },
-        { name :'/b',path: '/b', component: CompB ,alias:'/alias'},//别名alias，路由去b或者alias都是CompB组件
-        { path: '/a', redirect: '/b' },//重定向，a 默认跳转到 b
-        { path: '/c', redirect: { name: '/b' } },//也可以是一个命名的路由：
+        { path: '/user/:id', component: User, props: true },
+
+        // 对于包含命名视图的路由，你必须分别为每个命名视图添加 `props` 选项：
         {
-            path: '/d', redirect: () => {
-                return '/b'
-            }
+            path: '/user/:id',
+            components: { default: User, sidebar: Sidebar },
+            props: { default: true, sidebar: false }
         }
     ]
 })
